1beb8bac525b7b9e307d7d0a20c79dea445abb79
MirkoValle <mirko.valle20@gmail.com>
2024-05-13 15:23:10 +0200

    Added array with email

---
+++ Mail/js/script.js
@@ -0,0 +1,14 @@
+//-Mail
+//Chiedi all’utente la sua email,
+//controlla che sia nella lista di chi può accedere,
+//stampa un messaggio appropriato sull’esito del controllo.
+
+//Creo un array con una serie di email ipoteticamente salvate nel sistema
+//Chiedo all'utente di inserire la propria email
+//Controllo se l'email inserita fa parte nel gruppo interno l'array
+//Se l'email è presente allora risposta postiva, sennò risposta negativa
+
+
+
+const emailSalvate = ["perseo.orione@gmail.com", "paolo.latorre@gmail.com", "giannipinto27@outlook.it", "chiccogatto8899@icloud.com"]
+
